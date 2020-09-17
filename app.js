c8ac957f3c69fcf457dc37bba654cd90d545e76c
PranishShresth <yunche64@gmail.com>
2020-09-18 01:25:31 +1000

    testing

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
