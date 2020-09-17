7b9a3ea3d7e2526e4bfab552f10b136dcc030f9f
PranishShresth <yunche64@gmail.com>
2020-09-17 22:17:11 +1000

    Amended

---
+++ client/build/precache-manifest.ff6467d450690eb6ca9813f6e4b23697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79943a2319908465f232f298ca5f6214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35bc13e6958e602e2d1a",
+    "url": "/static/css/main.ed56dd79.chunk.css"
+  },
+  {
+    "revision": "bfbbab169b8e36c0fc7e",
+    "url": "/static/js/2.0503f00c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0503f00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35bc13e6958e602e2d1a",
+    "url": "/static/js/main.95cddb88.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
