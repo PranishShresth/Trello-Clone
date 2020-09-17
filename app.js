7b0cd98836452ad4910e584573db95fabac04e11
PranishShresth <yunche64@gmail.com>
2020-09-17 22:45:41 +1000

    amended

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 app.use(cors());
 app.use("/api", routes);
